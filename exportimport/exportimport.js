67bd0b846679bbdbf671c5c1ea69d4a3a0e69c7f
Sanjukta222 <sanjuktasasmal2002@gmail.com>
2026-01-11 15:46:23 +0530

    Add export and import

---
+++ exportimport/exportimport.js
@@ -0,0 +1,12 @@
+const getName = () =>{
+    return "sanjukta";
+}
+
+const getAge = () =>{
+    return "25";
+}
+// exports.getAge = getAge; only one fun to export
+module.exports={
+    getName,
+    getAge
+}
---
+++ exportimport/index.js
@@ -0,0 +1,6 @@
+ // const s1 = require("./exportimport");
+ // console.log(s1.getAge);
+ 
+ const {getName, getAge}= require("./exportimport");
+ console.log(getName());
+ 
